acfb1539006aad565fdb017019cb1956cd4c1c38
greatdad <85246406+greatdad@users.noreply.github.com>
2021-07-21 12:07:48 -0700

    Create ItemAdapter.kt

---
+++ app/src/main/java/com/gift/retrofit/ItemAdapter.kt
@@ -0,0 +1,34 @@
+package com.gift.retrofit
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.gift.retrofit.databinding.ItemViewBinding
+
+
+class ItemAdapter(var lists: List<Item>): RecyclerView.Adapter<ItemAdapter.ItemsDataViewHolder>() {
+
+    inner class ItemsDataViewHolder(val binding: ItemViewBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(list: Item){
+            binding.textView.text = list.name
+            binding.textView2.text = list.price.toString()
+            binding.textView1.text = list.id.toString()
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsDataViewHolder {
+        val binding: ItemViewBinding = ItemViewBinding.inflate(LayoutInflater.from(parent.context))
+        return ItemsDataViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemsDataViewHolder, position: Int) {
+        val singleItemsData = lists.get(position)
+        holder.bind(singleItemsData)
+    }
+
+    override fun getItemCount(): Int {
+        return lists.size
+    }
+}
