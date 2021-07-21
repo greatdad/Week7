441a7d5d424cd40b65967a953f5cf7790c9fa45f
greatdad <85246406+greatdad@users.noreply.github.com>
2021-07-21 12:01:27 -0700

    Update MainViewModel.kt

--- app/src/main/java/com/gift/retrofit/MainViewModel.kt
+++ app/src/main/java/com/gift/retrofit/MainViewModel.kt
@@ -1,21 +1,27 @@
 package com.gift.retrofit
 
+
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+
 class MainViewModel: ViewModel() {
+    val items: MutableLiveData<List<Item>> = MutableLiveData()
+    val students = MutableLiveData<List<Student>>()
 
-    val items: MutableLiveData<List<Todo>> = MutableLiveData()
-
-    fun getAllTodos(){
-        CoroutineScope(Dispatchers.IO).launch {
-            val todos: List<Todo> = RetrofitProvider.service.getAllTodos()
-            items.postValue(todos)
+    fun getAllItems(){
+        CoroutineScope(Dispatchers.IO).launch{
+            val items :List<Item> = RetrofitProvider.service.getAllItems()
+            this@MainViewModel.items.postValue(items)
         }
-
     }
 
+    fun addStudent(newStudent: Student) {
+        CoroutineScope(Dispatchers.IO).launch {
+            students.postValue(RetrofitProvider.service.addStudents(newStudent))
+        }
+    }
 }
