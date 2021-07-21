fc37388631ce51970bbac39ba0ec6c6bd3da4f85
greatdad <85246406+greatdad@users.noreply.github.com>
2021-07-21 11:59:12 -0700

    Update MainActivity.kt

--- app/src/main/java/com/gift/retrofit/MainActivity.kt
+++ app/src/main/java/com/gift/retrofit/MainActivity.kt
@@ -1,41 +1,38 @@
 package com.gift.retrofit
 
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.gift.retrofit.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var todoAdapter: TodoAdapter
+    private lateinit var itemAdapter: ItemAdapter
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        todoAdapter = TodoAdapter(listOf())
-        binding.recycView.adapter = todoAdapter
+        itemAdapter = ItemAdapter(listOf())
+        binding.recycler.adapter = itemAdapter
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-
-
         viewModel.apply {
-            getAllTodos()
-            items.observe(this@MainActivity, { todos ->
-                todoAdapter.list = todos
-                todoAdapter.notifyDataSetChanged()
+            getAllItems()
+            items.observe(this@MainActivity, {items ->
+                itemAdapter.lists = items
+                itemAdapter.notifyDataSetChanged()
             })
+        }
+
+        binding.button.setOnClickListener {
+            val myIntent = Intent(this, StudentAddActivity::class.java)
+            startActivity(myIntent)
         }
     }
 }
+
